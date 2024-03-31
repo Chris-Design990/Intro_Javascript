c2ac61dbd43e0721784117e582ddc1d4595bb22e
Chris-Design990 <78225768+Chris-Design990@users.noreply.github.com>
2024-03-31 17:10:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+//Q1:
+
+if string1 == string2:
+	return "equal"
+else:
+	return "not equal"
+
+//Q2:
+
+if number1 == number2:
+	return "equal"
+else if number1 >= number2:
+	return "greater than"
+else if number1 <= number2:
+	return "less than"
+
+//Q3
+
+function stringcombine(string1, string2){
+	console.log("")
+	return stirng1 + string2;
+}
+
+
+//Q4
+
+function cake(num){
+
+	if (num == 1){
+		return "5 dollars"
+	}
+	else if (num == 2){
+		return "10 dollars"
+	}
+	else if (num == 3){
+		return "15 dollars"
+	}
+	else {
+		return "We are out of cakes"
+	}
+}
+
